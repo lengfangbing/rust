7214f222a32f1fcbe989a909d5b6bbae4163ac9f
lengfangbing <lengfangbing@bytedance.com>
2020-08-25 11:08:04 +0800

    feat: update closures

--- rust_basic_grammar/src/closures/closures.rs
+++ rust_basic_grammar/src/closures/closures.rs
@@ -1,5 +1,7 @@
 use rand;
 use rand::Rng;
+use std::time::Duration;
+use std::thread;
 
 fn get_random_number() -> Result<f32, ()> {
     let mut rng = rand::thread_rng();
@@ -29,6 +31,47 @@ pub fn test_closures () {
         }
     };
     if let Ok(val) = float {
-        println!("{}", suggestion_result(val))
+        println!("{}", suggestion_result(val));
+        struct_closures(val);
+    }
+}
+
+struct Cache<T>
+    where T: Fn(f32) -> f32
+{
+    calculation: T,
+    value: Option<f32>,
+}
+
+impl<T> Cache<T>
+    where T: Fn(f32) -> f32
+{
+    fn new(calc: T) -> Cache<T> {
+        Cache{
+            calculation: calc,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: f32) -> f32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn struct_closures(val: f32) {
+    let mut result = Cache::new(|num| {
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+    if val < 0.5 {
+        println!("calculation result is less than 0.5: {}", result.value(val));
+    } else {
+        println!("calculation result is more than 0.5: {}", result.value(val));
     }
 }
